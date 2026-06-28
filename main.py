f7983c111f1cc2a61be6058fa2faa9a65c64884f
Abdelrahman Nader <abdelrahmannohman33@gmail.com>
2026-06-28 13:04:14 +0300

    Player appears on screen

--- main.py
+++ main.py
@@ -1,21 +1,31 @@
 from constants import *
 from logger import log_state
-
+from player import Player
 import pygame
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0.0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
-    while True:
-        log_state()
-        for event in pygame.event.get():
+    while True: #Game loop
+        log_state() #state logger
+        
+        for event in pygame.event.get(): #event tracker?
+            
             if event.type == pygame.QUIT:
                 return
-        screen.fill("blue")
+
+        dt = clock.tick(60) / 1000 #delta time          
+        #print(dt)
+
+        #order is extremely important here
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
-        
-    
+
+
 if __name__ == "__main__":
     main()
